9de1f0bbba9b1aa403b7ebad00583f5dee8ad588
Pablo Ibanez <pablo.ibanez@eurotransportcar.com>
2025-03-24 12:34:37 +0100

    add athletes for players of games

--- packages/api/player/domain/models/player.ts
+++ packages/api/player/domain/models/player.ts
@@ -1,6 +1,9 @@
 import PlayerId from './id'
 import User from '~/user/domain/models/user'
 import Game from '~/game/domain/models/game'
+import Athlete from '~/athlete/domain/models/athlete'
+import AthleteId from '~/athlete/domain/models/id'
+
 
 class Player {
   private _id: PlayerId
@@ -8,13 +11,17 @@ class Player {
   private _game: Game
   private _ranking: number
   private _cash: number
+  private _athletes: Athlete[]
+
+  private static readonly MAX_ATHLETES = 16
 
   private constructor(id: PlayerId, user: User, game: Game, cash: number) {
     this._id = id
     this._user = user
     this._game = game
-    this._ranking = 0 
-    this._cash = cash // Se inicializa con la cantidad asignada en GameConfig
+    this._ranking = 0
+    this._cash = cash
+    this._athletes = []
   }
 
   static create({ id, user, game, cash }: { id: PlayerId; user: User; game: Game; cash: number }) {
@@ -41,6 +48,10 @@ class Player {
     return this._cash
   }
 
+  get athletes(): Athlete[] {
+    return this._athletes
+  }
+
   set ranking(position: number) {
     if (position < 1) throw new Error('Ranking must be greater than 0')
     this._ranking = position
@@ -55,6 +66,17 @@ class Player {
     if (amount < 0) throw new Error('Amount must be positive')
     if (this._cash < amount) throw new Error('Not enough cash')
     this._cash -= amount
+  }
+
+  addAthlete(athlete: Athlete) {
+    if (this._athletes.length >= Player.MAX_ATHLETES) {
+      throw new Error(`Cannot add more than ${Player.MAX_ATHLETES} athletes`)
+    }
+    this._athletes.push(athlete)
+  }
+
+  removeAthlete(athleteId: AthleteId) {
+    this._athletes = this._athletes.filter(athlete => athlete.id !== athleteId)
   }
 }
 
